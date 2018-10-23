b07ff1788f74dd6bddbd553175a42ef668f07c9b
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-23 09:34:39 +0300

    create class post form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,15 +1,15 @@
-# from flask_wtf import FlaskForm
-# from wtforms import StringField,TextAreaField,SubmitField,SelectField
-# from wtforms.validators import Required,Email,EqualTo
-# from ..models import Subscriber
-# from wtforms import ValidationError
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import Subscriber
+from wtforms import ValidationError
 
 
-# class PostForm(FlaskForm):
-#     title = StringField("Post Title",validators=[Required()])
-#     post = TextAreaField("Write your post here")
-#     category = SelectField("Post Category",choices=[('Tech','Tech'),('Travel','Travel'),('Fashion','Fashion'),('Food','Food'),('Life','Life'),('Culture','Culture'),('Fatherhood','Fatherhood'),('People','People')],validators=[Required()])
-#     submit = SubmitField('Submit')
+class PostForm(FlaskForm):
+    title = StringField("Post Title",validators=[Required()])
+    post = TextAreaField("Write your post here")
+    category = SelectField("Post Category",choices=[('Tech','Tech'),('Travel','Travel'),('Fashion','Fashion'),('Food','Food'),('Life','Life'),('Culture','Culture'),('Fatherhood','Fatherhood'),('People','People')],validators=[Required()])
+    submit = SubmitField('Submit')
 
 # class SubscriberForm(FlaskForm):
 #     email = StringField("Email Address",validators=[Required(),Email()])
