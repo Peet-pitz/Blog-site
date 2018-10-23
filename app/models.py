ae131b45f9ee55c0f3707812c042986ea3349777
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-23 09:50:49 +0300

    crerate class role

--- app/models.py
+++ app/models.py
@@ -85,12 +85,12 @@ class Comment(db.Model):
         db.session.commit()
 
 
-# class Role(db.Model):
-#     __tablename__ = 'roles'
+class Role(db.Model):
+    __tablename__ = 'roles'
 
-#     id = db.Column(db.Integer,primary_key=True)
-#     name = db.Column(db.String(255))
-#     users= db.relationship('User',backref='role',lazy="dynamic")
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+    users= db.relationship('User',backref='role',lazy="dynamic")
 
-#     def __repr__(self):
-#         return f'User{self.name}'
+    def __repr__(self):
+        return f'User{self.name}'
