53b42abad0fb5b83d3f78cd68444235962575915
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-23 09:49:22 +0300

    create class comment

--- app/models.py
+++ app/models.py
@@ -62,19 +62,19 @@ class Post(db.Model):
         db.session.add(self)
         db.session.commit()
 
-#     @classmethod
-#     def get_posts(cls,id):
-#         posts=Post.query.filter_by(post_id=id).all()
-#         return posts
+    @classmethod
+    def get_posts(cls,id):
+        posts=Post.query.filter_by(post_id=id).all()
+        return posts
 
-# class Comment(db.Model):
-#     __tablename__='comments'
+class Comment(db.Model):
+    __tablename__='comments'
 
-#     id = db.Column(db.Integer,primary_key = True)
-#     comment = db.Column(db.String)
-#     posted = db.Column(db.DateTime,default=datetime.utcnow)
-#     post_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
-#     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String)
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    post_id = db.Column(db.Integer,db.ForeignKey("posts.id"))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
 
 #     def save_comment(self):
 #         db.session.add(self)
