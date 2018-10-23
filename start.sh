943f3a6c6547655a79907d5b136a676065726608
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-23 09:54:05 +0300

    push 2

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-# set SECRET_KEY="flasksecret"
+set SECRET_KEY="flasksecret"
 
-# set MAIL_USERNAME="jack.sin46@gmail.com"
-# set MAIL_PASSWORD="alienboy"
+set MAIL_USERNAME="jack.sin46@gmail.com"
+set MAIL_PASSWORD="alienboy"
 
-# set DATABASE_URL ='postgresql+psycopg2://postgres:123@localhost/blog'
+set DATABASE_URL ='postgresql+psycopg2://postgres:123@localhost/blog'
 
-# py manage.py server
+py manage.py server
