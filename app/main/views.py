c0af2b1574124aa5b9b8908d1d2e355b5f18efe0
Peter Njoroge <njoroge.pitz@gmail.com>
2018-10-23 09:36:21 +0300

    create subscribe function and email

--- app/main/views.py
+++ app/main/views.py
@@ -1,30 +1,30 @@
-# from flask import render_template,request,redirect,url_for,abort
-# from . import main
-# from .forms import PostForm,SubscriberForm,CommentForm
-# from ..import db,photos
-# from ..models import User,Post,Role,Subscriber,Comment
-# from flask_login import login_required,current_user
-# import markdown2
-# from ..email import mail_message
+from flask import render_template,request,redirect,url_for,abort
+from . import main
+from .forms import PostForm,SubscriberForm,CommentForm
+from ..import db,photos
+from ..models import User,Post,Role,Subscriber,Comment
+from flask_login import login_required,current_user
+import markdown2
+from ..email import mail_message
 
-# #Views
-# @main.route("/",methods=['GET','POST'])
-# def index():
-#     """
-#     View root page function that returns the index page and its data
-#     """
-#     posts = Post.query.all()
-#     form = SubscriberForm()
-#     if form.validate_on_submit():
-#         email = form.email.data
+#Views
+@main.route("/",methods=['GET','POST'])
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    posts = Post.query.all()
+    form = SubscriberForm()
+    if form.validate_on_submit():
+        email = form.email.data
 
-#         new_subscriber=Subscriber(email=email)
-#         new_subscriber.save_subscriber()
+        new_subscriber=Subscriber(email=email)
+        new_subscriber.save_subscriber()
 
-#         mail_message("Subscription Received","email/welcome_subscriber",new_subscriber.email,subscriber=new_subscriber)
+        mail_message("Subscription Received","email/welcome_subscriber",new_subscriber.email,subscriber=new_subscriber)
 
-#     title = "Welcome to My Blog"
-#     return render_template('index.html',title=title,posts=posts,subscriber_form=form)
+    title = "Welcome to My Blog"
+    return render_template('index.html',title=title,posts=posts,subscriber_form=form)
 
 # @main.route("/new_post",methods=['GET','POST'])
 # @login_required
